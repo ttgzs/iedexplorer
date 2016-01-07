6a69a01ae6241cd28ebeca626d012609bc2163b1
Pavel <pavel-ch@users.sf.net>
2016-01-07 23:28:14 +0100

    Starting iec Model tree

--- Iec61850Model.cs
+++ Iec61850Model.cs
@@ -12,6 +12,10 @@ namespace IEDExplorer
         /// </summary>
         public NodeIed ied = new NodeIed("ied");
         /// <summary>
+        /// Server data ordered by IEC61850 data model
+        /// </summary>
+        public NodeIed iec = new NodeIed("iec");
+        /// <summary>
         /// Server named variable lists
         /// </summary>
         public NodeIed lists = new NodeIed("lists");
@@ -34,10 +38,34 @@ namespace IEDExplorer
         public Iec61850Model(Iec61850State iecs)
         {
             (ied as NodeIed).iecs = iecs;
+            (iec as NodeIed).iecs = iecs;
             (lists as NodeIed).iecs = iecs;
             (files as NodeIed).iecs = iecs;
             (urcbs as NodeIed).iecs = iecs;
             (brcbs as NodeIed).iecs = iecs;
+        }
+
+        public void BuildIECModelFromMMSModel()
+        {
+            foreach (NodeLD ld in ied.GetChildNodes())      // LD level
+            {
+                NodeLD ild = new NodeLD(ld.Name);
+                ild = (NodeLD)iec.AddChildNode(ild);
+                foreach (NodeLN ln in ld.GetChildNodes())   // LN level
+                {
+                    NodeLN iln = new NodeLN(ln.Name);
+                    iln = (NodeLN)ild.AddChildNode(iln);
+                    foreach (NodeFC fc in ln.GetChildNodes())   // FC level - skipping
+                    {
+                        // keep knowing FC for DA
+                        foreach (NodeDO dO in fc.GetChildNodes())   // DO level
+                        {
+                            NodeDO ido = new NodeDO(dO.Name);
+                            ido = (NodeDO)iln.AddChildNode(ido);
+                        }
+                    }
+                }
+            }
         }
     }
 }
